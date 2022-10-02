baf72a45d1d4d7b3c2fdfba93176b305d9048134
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-10-02 20:35:45 +0700

    apply jwt

---
+++ server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @param  LoginRequest  $request
+     * @return JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->input(['email', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json([
+                    'status' => false,
+                    'error' => 'Unauthorized'
+                ]
+                , ResponseAlias::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Successfully logged out'
+        ], ResponseAlias::HTTP_OK);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string  $token
+     *
+     * @return JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'status' => true,
+            'data' => [
+                'access_token' => $token,
+                'token_type' => 'bearer',
+                'expires_in' => auth()->factory()->getTTL() * 60
+            ],
+        ], ResponseAlias::HTTP_OK);
+    }
+}
