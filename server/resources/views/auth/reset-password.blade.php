0bef1388d63efb6ab153e7cbde0db8c8822a7d36
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-10-04 22:34:45 +0700

    Create reset-password.blade.php

---
+++ server/resources/views/auth/reset-password.blade.php
@@ -0,0 +1,66 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<div class="form-gap"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="text-center">
+                        <h3><i class="fa fa-lock fa-4x"></i></h3>
+                        <h2 class="text-center">{{$email}}</h2>
+                        <div class="panel-body">
+
+                            <form id="reset-password-form" role="form" autocomplete="off" class="form" method="post"
+                                  action="{{route('password.update')}}">
+
+                                <div class="form-group">
+                                    <div class="input-group" style="padding-bottom: 0.75em">
+                                        <span class="input-group-addon"><i
+                                                class="glyphicon glyphicon-envelope color-blue"></i></span>
+                                        <input id="email" name="email" placeholder="Email address" class="form-control"
+                                               type="email" required>
+                                        @if ($errors->has('email'))
+                                            <span class="text-danger">{{ $errors->first('email') }}</span>
+                                        @endif
+                                    </div>
+                                    <div class="input-group" style="padding-bottom: 0.75em">
+                                        <span class="input-group-addon"><i
+                                                class="glyphicon glyphicon-envelope color-blue"></i></span>
+                                        <input id="password " name="password " placeholder="New password"
+                                               class="form-control" type="password" required>
+                                        @if ($errors->has('password'))
+                                            <span class="text-danger">{{ $errors->first('password') }}</span>
+                                        @endif
+                                    </div>
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><i
+                                                class="glyphicon glyphicon-envelope color-blue"></i></span>
+                                        <input id="password_confirmation " name="password_confirmation "
+                                               placeholder="Confirm new password" class="form-control" type="password"
+                                               required>
+                                        @if ($errors->has('password_confirmation'))
+                                            <span
+                                                class="text-danger">{{ $errors->first('password_confirmation') }}</span>
+                                        @endif
+                                    </div>
+                                    <input name="token" type="hidden" value="{{$token}}">
+                                    @csrf
+                                </div>
+                                <div class="form-group">
+                                    <input name="recover-submit" class="btn btn-lg btn-primary btn-block"
+                                           value="Submit" type="submit">
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
