902056727369eb2b3db348073823194f5c653930
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-10-04 22:35:15 +0700

    set locale to vietnam

--- server/app/Providers/AppServiceProvider.php
+++ server/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+//        ResetPassword::createUrlUsing(function ($user, string $token) {
+//            return 'https://example.com/reset-password?token='.$token.'/'.$user->email;
+//        });
+        App::setLocale('vi');
     }
 }
