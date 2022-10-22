a6109b53f18d40f134eb2cf16cf9ccd05b787fe5
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-10-22 21:15:14 +0700

    optimize code

--- server/app/Http/Controllers/ComicsController.php
+++ server/app/Http/Controllers/ComicsController.php
@@ -16,14 +16,12 @@ class ComicsController extends Controller
     protected $comics;
     protected $category;
     protected $author;
-    protected $comics_category;
 
     public function __construct()
     {
         $this->category = new Category;
         $this->comics = new Comics;
         $this->author = new Author;
-        $this->comics_category = new ComicsCategory;
     }
 
     /**
@@ -34,7 +32,7 @@ class ComicsController extends Controller
     public function index()
     {
         return response()->json([
-            'list' => $this->comics->getAll(),
+            'list' => Comics::all(),
         ]);
     }
 
@@ -42,7 +40,7 @@ class ComicsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param Request $request
+     * @param  Request  $request
      * @return JsonResponse
      */
     public function store(Request $request)
@@ -56,29 +54,31 @@ class ComicsController extends Controller
                 'category_id.*' => 'exists:categories,id,deleted_at,NULL',
 
             ]);
-            $comics = $this->comics->create($request->only(['name', 'published_date', 'author_id', 'description', 'status']));
+            $comics = $this->comics->create($request->only([
+                'name', 'published_date', 'author_id', 'description', 'status'
+            ]));
             foreach ($request->category_id as $value) {
-                $this->comics_category->create([
+                ComicsCategory::create([
                     'comic_id' => $comics->id,
                     'category_id' => $value,
                 ]);
             };
             return response()->json([
                 'message' => 'Add success comics!',
-                'comics'=> $comics
+                'comics' => $comics
             ]);
         } catch (Throwable $err) {
             return response()->json([
                 'message' => 'Add error comics!',
                 'error' => $err->getMessage(),
-            ],Response::HTTP_BAD_REQUEST);
+            ], Response::HTTP_BAD_REQUEST);
         }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param Comics $comic
+     * @param  Comics  $comic
      * @return JsonResponse
      */
     public function show(Comics $comic)
@@ -92,9 +92,9 @@ class ComicsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param Request $request
-     * @param Comics $comic
-     * @return \Illuminate\Http\Response
+     * @param  Request  $request
+     * @param  Comics  $comic
+     * @return JsonResponse
      */
     public function update(Request $request, Comics $comic)
     {
@@ -102,14 +102,14 @@ class ComicsController extends Controller
             $validated = $request->validate([
                 'name' => 'required|max:255',
                 'published_date' => 'required',
-                'author_id' =>'required|exists:authors,id,deleted_at,NULL',
+                'author_id' => 'required|exists:authors,id,deleted_at,NULL',
                 'category_id' => 'required|array',
                 'category_id.*' => 'exists:categories,id,deleted_at,NULL',
             ]);
             $comic->update($request->only(['name', 'published_date', 'author_id', 'description', 'status']));
-            $this->comics_category->deletes($comic->id);
+            ComicsCategory::deletes($comic->id);
             foreach ($request->category_id as $value) {
-                $this->comics_category->create([
+                ComicsCategory::create([
                     'comic_id' => $comic->id,
                     'category_id' => $value,
                 ]);
@@ -131,26 +131,23 @@ class ComicsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param Comics $comic
-     * @return \Illuminate\Http\Response
+     * @param  Comics  $comic
+     * @return JsonResponse
      */
     public function destroy(Comics $comic)
     {
-//       $comic->delete();
-//        return response()->json([
-//            'status' => true,
-//            'message' => 'Delete success comics!'
-//        ]);
+        $comic->delete();
         return response()->json([
-            'message' => $comic->category,
+            'status' => true,
+            'message' => 'Delete success comics!'
         ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param Category $category
-     * @return \Illuminate\Http\Response
+     * @param  Category  $category
+     * @return JsonResponse
      */
     public function showCategory(Category $category)
     {
--- server/app/Models/ComicsCategory.php
+++ server/app/Models/ComicsCategory.php
@@ -9,15 +9,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class ComicsCategory extends Model
 {
     use HasFactory;
-    protected $table="comic_category";
-    protected $fillable = ['comic_id','category_id'];
+
+    protected $table = "comic_category";
+    protected $fillable = ['comic_id', 'category_id'];
 
 
-    static function getAllComics($comics_id){
-        return ComicsCategory::where('comic_id',$comics_id)->get();
+    static function getAllComics($comics_id)
+    {
+        return self::where('comic_id', $comics_id)->get();
     }
-    function deletes($comics_id){
-        return ComicsCategory::where('comic_id',$comics_id)->delete();
+
+    static function deletes($comics_id)
+    {
+        return self::where('comic_id', $comics_id)->delete();
     }
 
 }
