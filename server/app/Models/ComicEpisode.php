80375e8e81a31f38dddc0d1baf71cbe320b37d1f
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-10-22 14:05:23 +0700

    add fillable and trait to model

--- server/app/Models/ComicEpisode.php
+++ server/app/Models/ComicEpisode.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use App\Http\Traits\AddUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ComicEpisode extends Model
 {
-    use HasFactory;
+    use HasFactory, AddUser;
+
+    protected $fillable = [
+        'comic_id',
+        'episode_number',
+        'published_date',
+        'likes',
+        'views'
+    ];
 }
