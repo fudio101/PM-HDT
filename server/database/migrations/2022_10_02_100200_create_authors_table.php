478e8752aed3b35b8944d27ef7c77db6176c9afb
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-11-04 22:08:09 +0700

    update tables structure

--- server/database/migrations/2022_10_02_100200_create_authors_table.php
+++ server/database/migrations/2022_10_02_100200_create_authors_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->foreignId('user_id')->constrained();
             $table->softDeletes();
             $table->timestamps();
