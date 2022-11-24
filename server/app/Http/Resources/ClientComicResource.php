5d460480125720d6fd19d11bff8f722a25bebd9a
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-11-24 22:06:42 +0700

    fix api data structure

---
+++ server/app/Http/Resources/ClientComicResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClientComicResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array|Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+//        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'published_date' => $this->published_date,
+            'status' => $this->status,
+            'updated_time_diff' => $this->updated_time_diff,
+            'image_url' => $this->image_url,
+            'author' => $this->author,
+            'categories' => $this->categories,
+        ];
+    }
+}
