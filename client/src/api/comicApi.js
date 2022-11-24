b29fd8a92271408c22393ac6e7b67b481da437ba
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-11-24 23:22:49 +0700

    fix layout

--- client/src/api/comicApi.js
+++ client/src/api/comicApi.js
@@ -2,14 +2,14 @@ import axiosClient from "./axiosClient";
 
 const comicApi = {
     search: (keywork) =>
-        axiosClient.get("/search", {
+        axiosClient.get("get-comic/search", {
             params: {
                 search: keywork,
             },
         }),
-    getComic: (slug) => axiosClient.get(`/${slug}`),
+    getComic: (slug) => axiosClient.get(`get-comic/${slug}`),
     getNewComic: (number) =>
-        axiosClient.get("get-just-updated-comics", {
+        axiosClient.get("get-comic/get-just-updated-comics", {
             params: {
                 number: number,
             },
