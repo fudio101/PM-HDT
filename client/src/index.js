45ae5685fe484d5b0e07effc135f01bb893fc3b1
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-11-28 21:57:22 +0700

    config redux-persist

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
 import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+let persistor = persistStore(store);
+
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     </React.StrictMode>
 );
