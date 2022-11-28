9bf7dc60556f376052f3dea08af04374285bd6dd
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-11-29 05:00:57 +0700

    add footer logo

--- client/src/components/layouts/footer/Footer.js
+++ client/src/components/layouts/footer/Footer.js
@@ -1,34 +1,46 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import classes from "./Footer.module.css";
-function Footer() {
-  return (
-    <footer className={classes.footer}>
-      <ul className={classes.footer__nav}>
-        <li className={classes.footer__item}>
-          <NavLink className={classes.footer__link}>About</NavLink>
-        </li>
-        <li className={classes.footer__item}>
-          <NavLink className={classes.footer__link}>Pricing</NavLink>
-        </li>
-        <li className={classes.footer__item}>
-          <NavLink className={classes.footer__link}>Terms of Use</NavLink>
-        </li>
-        <li className={classes.footer__item}>
-          <NavLink className={classes.footer__link}>Privacy Policy</NavLink>
-        </li>
+import logo from "../logo.png";
 
-        <li className={classes.footer__item}>
-          <NavLink className={classes.footer__link}>Contact Us</NavLink>
-        </li>
-      </ul>
-      <div className={classes.footer__logo}>LOGO</div>
-      <p className={classes.footer__copyright}>
-        &copy; Copyright by Group PMHDT
-      </p>
-    </footer>
-  );
+function Footer() {
+    return (
+        <footer className={classes.footer}>
+            <ul className={classes.footer__nav}>
+                <li className={classes.footer__item}>
+                    <NavLink className={classes.footer__link}>About</NavLink>
+                </li>
+                <li className={classes.footer__item}>
+                    <NavLink className={classes.footer__link}>Pricing</NavLink>
+                </li>
+                <li className={classes.footer__item}>
+                    <NavLink className={classes.footer__link}>
+                        Terms of Use
+                    </NavLink>
+                </li>
+                <li className={classes.footer__item}>
+                    <NavLink className={classes.footer__link}>
+                        Privacy Policy
+                    </NavLink>
+                </li>
+
+                <li className={classes.footer__item}>
+                    <NavLink className={classes.footer__link}>
+                        Contact Us
+                    </NavLink>
+                </li>
+            </ul>
+            <div className={classes.footer__logo}>
+                <Link to={"/"} className={classes.logo_box}>
+                    <img className={classes.logo} src={logo} alt="Logo" />
+                </Link>
+            </div>
+            <p className={classes.footer__copyright}>
+                &copy; Copyright by Group PMHDT
+            </p>
+        </footer>
+    );
 }
 
 export default Footer;
