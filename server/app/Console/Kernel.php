cfe2a832efe6423934387d93e33c3941ddc3386b
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-12-03 22:13:59 +0700

    view statistics by day

--- server/app/Console/Kernel.php
+++ server/app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Models\ComicEpisodeView;
+use App\Models\ComicEpisodeViewByDay;
 use App\Models\ComicEpisodeViewByHour;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -21,13 +22,14 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('auth:clear-resets')->everyFourHours();
-        
+
         $schedule->call(function () {
-            $startTime = Carbon::make(now())->subHours()->microsecond(0)->second(0)->minute(0);
+            $startTime = Carbon::make(now())->subHour()->microsecond(0)->second(0)->minute(0);
             $endTime = Carbon::make(now())->microsecond(0)->second(0)->minute(0)->subMicroseconds();
             $hourViews = ComicEpisodeView::query()
                 ->where('created_at', '>=', $startTime)
                 ->where('created_at', '<=', $endTime)
+                ->where('accepted', '=', 1)
                 ->groupBy('comic_episode_id', 'ip')
                 ->select('comic_episode_id', DB::raw('count(*) as total'))
                 ->get();
@@ -38,6 +40,23 @@ class Kernel extends ConsoleKernel
                 $comicEpisodeViewByHour->save();
             }
         })->hourlyAt(0);
+
+        $schedule->call(function () {
+            $startTime = Carbon::make(now())->subDay()->microsecond(0)->second(0)->minute(0)->hour(0);
+            $endTime = Carbon::make(now())->microsecond(0)->second(0)->minute(0)->hour(0)->subMicroseconds();
+            $dayViews = ComicEpisodeViewByHour::query()
+                ->where('created_at', '>=', $startTime)
+                ->where('created_at', '<=', $endTime)
+                ->groupBy('comic_episode_id')
+                ->select('comic_episode_id', DB::raw('sum(views) as total'))
+                ->get();
+            foreach ($dayViews as $dayView) {
+                $comicEpisodeViewByDay = new ComicEpisodeViewByDay();
+                $comicEpisodeViewByDay->comic_episode_id = $dayView->comic_episode_id;
+                $comicEpisodeViewByDay->views = $dayView->total;
+                $comicEpisodeViewByDay->save();
+            }
+        })->dailyAt("00:00");
     }
 
     /**
