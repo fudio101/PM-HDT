37dc995afae20730c51124bd5efa69114e2d3f81
Nguyễn Đỗ Thế Nguyên <38380477+fudio101@users.noreply.github.com>
2022-12-03 16:41:07 +0700

    view statistics by hour

--- server/app/Console/Kernel.php
+++ server/app/Console/Kernel.php
@@ -2,21 +2,42 @@
 
 namespace App\Console;
 
+use App\Models\ComicEpisodeView;
+use App\Models\ComicEpisodeViewByHour;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  Schedule  $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('auth:clear-resets')->everyFourHours();
+        
+        $schedule->call(function () {
+            $startTime = Carbon::make(now())->subHours()->microsecond(0)->second(0)->minute(0);
+            $endTime = Carbon::make(now())->microsecond(0)->second(0)->minute(0)->subMicroseconds();
+            $hourViews = ComicEpisodeView::query()
+                ->where('created_at', '>=', $startTime)
+                ->where('created_at', '<=', $endTime)
+                ->groupBy('comic_episode_id', 'ip')
+                ->select('comic_episode_id', DB::raw('count(*) as total'))
+                ->get();
+            foreach ($hourViews as $hourView) {
+                $comicEpisodeViewByHour = new ComicEpisodeViewByHour();
+                $comicEpisodeViewByHour->comic_episode_id = $hourView->comic_episode_id;
+                $comicEpisodeViewByHour->views = $hourView->total;
+                $comicEpisodeViewByHour->save();
+            }
+        })->hourlyAt(0);
     }
 
     /**
